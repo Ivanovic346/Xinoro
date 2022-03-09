62a198b0d5ea64a7603ef5a700f91e671bdc74fa
Egor Udovin <57674283+xoheveras@users.noreply.github.com>
2022-03-09 16:09:01 +0300

    update

---
+++ core/controllers/forumController.php
@@ -0,0 +1,18 @@
+<?php 
+
+	/*
+		Auto Generate file
+		GitHub https://github.com/xoheveras/xinoro
+	*/
+
+	class forumController extends Controller 
+	{
+		function indexAction()
+			{
+				$this->view->LoadDesign();
+			}
+
+			#input_region#
+	}
+
+?> 
