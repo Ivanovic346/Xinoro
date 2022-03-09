1e26e91dfa3eb5514b0e70993850fd6909796375
McVanhellm <mcvanhellm@gmail.com>
2022-03-09 22:12:16 +0300

    innition commit

--- core/views/cmsxinoro/test.php
+++ core/views/cmsxinoro/test.php
@@ -1,70 +1,80 @@
-<div index="header-1" class="xinoro-box">
-    <div id="xinoro_background" image-path="aGVhZGVyLTEvYmctaGVhZGVyLTEucG5n" class="content-first-header_1">
-        <div class="black-bg-header-1"></div>
-        <div class="introduction-header-1">
-            <div class="inner first_intro" id="xinoro_text_ed">
-                <p>OUR COMPANY</p>
+<div index="footer-6" class="xinoro-box">
+  <footer class="box-footer-6">           
+      <div class="container">
+          <div class="row">
+            <div class="col-6 col-lg-3 offset-lg-1 mb-3">
+                  <div class="logo" id="xinoro_text_ed">Xinoro</div> 
+                  <div class="caption" id="xinoro_text_ed">Designed and built with all the love in the world by the RetCode tehmhnmvntributors.</div>                        
             </div>
-            <div class="inner second_intro" id="xinoro_text_ed">
-                <p>Bring Your Ideas to Life</p>
-            </div>
-            <div class="inner third_intro" id="xinoro_text_ed">
-                <p>Everything that you dreamed of can be brought to life exactly at the </p>
-                <p>moment when you decide to win</p>
-            </div>
-            <div class="arrow">
-                <img src="vendor/assets/XinoroEditor/images/header-1/arrow-header-1.png">
-            </div>
-        </div>
-    </div>
-</div><div index="text-1" class="xinoro-box">
-    <div class="container-text-1">
-        <div class="title" id="xinoro_text_ed"><h1>WE LOVE OLEG</h1></div>
-        <div class="line"></div>
-        <div class="text" id="xinoro_text_ed">Фотоэффе́кт, или фотоэлектри́ческий эффе́кт, — явление взаимодействия света или любого другого электромагнитного излучения с веществом, при котором энергия фотонов передаётся электронам вещества. В конденсированных веществах выделяют внешний и внутренний фотоэффект.</div>
-        <div class="button">
-            <button id="xinoro_text_ed"><b>learn more</b></button>
-        </div>
-    </div>
-</div><div index="footer-1" class="xinoro-box">
-    <footer class="box-footer-1">        
-        <div class="container">  
-            <div class="row">
-                <div class="col-lg-3 mb-3">
-                    <div class="logo" id="xinoro_text_ed">Xinoro</div>
-                    <div class="caption" id="xinoro_text_ed">Designed and built with all the love in the world by the <a href="#">RetCode</a> team with the help of our contributors.<br><br>Code licensed MIT, docs CC BY 3.0.<br><br>Currently v5.1.3.</div>
+            <div class="col-6 col-lg-2 mb-3">
+                    <h6 class="prod" id="xinoro_text_ed">PRODUCTS</h6>
+                    <hr class="linia1">
+                    <ul class="list-unstyled">
+                        <li class="mb-2" id="xinoro_text_ed"><a href="#">MDBootstrap</a></li>
+                        <li class="mb-2" id="xinoro_text_ed"><a href="#">MDWordPress</a></li>
+                        <li class="mb-2" id="xinoro_text_ed"><a href="#">BrandFlow</a></li>
+                        <li class="mb-2" id="xinoro_text_ed"><a href="#">Bootstrap Angular</a></li>
+                    </ul>
                 </div>
-                <div class="col-6 col-lg-2 offset-lg-1 mb-3">
-                    <h5 id="xinoro_text_ed">Links</h5>
+                <div class="col-6 col-lg-2 mb-3">
+                    <h6 class="prod" id="xinoro_text_ed">USEFUL LINKS</h6>
+                    <hr class="linia1">
                     <ul class="list-unstyled">
-                      <li class="mb-2"><a href="#">Home</a></li>
-                      <li class="mb-2"><a href="#">Docs</a></li>
-                      <li class="mb-2"><a href="#">Examples</a></li>
-                      <li class="mb-2"><a href="#">Themes</a></li>
-                      <li class="mb-2"><a href="#">Blog</a></li>
+                        <li class="mb-2" id="xinoro_text_ed"><a href="#">Your Account</a></li>
+                        <li class="mb-2" id="xinoro_text_ed"><a href="#">Become an Affance</a></li>
+                        <li class="mb-2" id="xinoro_text_ed"><a href="#">Shipping Rates</a></li>
+                        <li class="mb-2" id="xinoro_text_ed"><a href="#">Help</a></li>
                     </ul>
-                  </div>
-                  <div class="col-6 col-lg-2 mb-3">
-                    <h5 id="xinoro_text_ed">Guides</h5>
+                </div>  
+                <div class="col-6 col-lg-2 mb-3">
+                    <h6 class="prod" id="xinoro_text_ed">CONTACT</h6>
+                    <hr class="linia1">
                     <ul class="list-unstyled">
-                      <li class="mb-2"><a href="#">Getting started</a></li>
-                      <li class="mb-2"><a href="#">Starter template</a></li>
-                      <li class="mb-2"><a href="#">Webpack</a></li>
-                      <li class="mb-2"><a href="#">Parcel</a></li>
+                        <li class="mb-2"><p id="xinoro_text_ed"><img class="home" src="vendor/assets/XinoroEditor/images/footer-6/home.svg" alt="home" weight="25px" height="25px">New York, NY 10012, US</p></li>
+                        <li class="mb-2"><p id="xinoro_text_ed"><img class="mail" src="vendor/assets/XinoroEditor/images/footer-6/mail.svg" alt="mail" weight="18px" height="18px">info@example.com</p></li>
+                        <li class="mb-2"><p id="xinoro_text_ed"><img class="phone" src="vendor/assets/XinoroEditor/images/footer-6/phone.svg" alt="phone" weight="23px" height="23px">+ 01 234 567 89</p></li>
                     </ul>
-                  </div>
-                  <div class="col-6 col-lg-2 mb-3">
-                    <h5 id="xinoro_text_ed">Community</h5>
-                    <ul class="list-unstyled">
-                      <li class="mb-2"><a href="#">Issues</a></li>
-                      <li class="mb-2"><a href="#">Discussions</a></li>
-                      <li class="mb-2"><a href="#">Corporate sponsors</a></li>
-                      <li class="mb-2"><a href="#">Open Collective</a></li>
-                      <li class="mb-2"><a href="#">Slack</a></li>
-                      <li class="mb-2"><a href="#">Stack Overflow</a></li>
-                    </ul>
-                  </div>
+                </div>                              
             </div>
-        </div>                                   
-    </footer>
+            <div class="teamblock">
+                <div class="team" id="xinoro_text_ed">© 2022 RedCode, Team.</div> 
+            </div>                                                 
+        </div>                                        
+      </footer></div><div index="menu-2" class="xinoro-box">
+    <nav class="menu-2-box">      
+        <div class="menu">
+          <ul class="clear">
+            <li>
+              <a href="index.html" title="Home">
+                <i class="fa fa-home" aria-hidden="true"></i>
+                <span class="link-text" id="xinoro_text_ed">Home</span>
+              </a>
+            </li>
+            <li>
+              <a href="index.html" title="Features">
+                <i class="fa fa-lightbulb-o" aria-hidden="true"></i>
+                <span class="link-text" id="xinoro_text_ed">Features</span>
+              </a>
+            </li>
+            <li>
+              <a href="index.html" title="Portfolio">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+                <span class="link-text" id="xinoro_text_ed">Portfolio</span>
+              </a>
+            </li>
+            <li>
+              <a href="index.html" title="Blog">
+                <i class="fa fa-comments-o" aria-hidden="true"></i>
+                <span class="link-text" id="xinoro_text_ed">Blog</span>
+              </a>
+            </li>
+            <li>
+              <a href="index.html" title="Contact">
+                <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                <span class="link-text" id="xinoro_text_ed">Contact</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+  </nav>
 </div>
